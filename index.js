a5c24dd7cde81db4841084164d63b62ba0ba03c3
anv6264 <49003246+anv6264@users.noreply.github.com>
2019-06-01 18:57:58 +0300

    Add files via upload
    just commit

---
+++ index.js
@@ -0,0 +1,9 @@
+//const camelCase = require('camelcase');
+
+import camelCase from 'camelcase';
+
+console.log(camelCase('foo-bar'));
+
+document.querySelector('div');
+
+div.addEventListener('change', )
